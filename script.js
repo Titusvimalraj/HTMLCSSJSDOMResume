42bd26fb24152e171a9b433cdd3197a395b814a4
Titus Vimal Raj <titusvimalraj@gmail.com>
2020-08-02 08:58:49 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,207 @@
+let resumeInnerHTML = `<main>
+
+
+<section id="profile-picture-section">
+    <img id="profile-picture" src="./Titusvimalraj_profile_picture.jpg" alt="No-dp"> <br><br>
+    <div id="profile-picture-band">
+    </div>
+    <div id="profile-picture-band-box">
+        <h1>TITUS VIMAL RAJ</h1>
+    </div>
+</section>
+<div id="profile-picture-section-contact">
+    Chennai, India | 8610265950 | titusvimalraj@gmail.com | titusvimalraj.github.io
+</div>
+<hr>
+<section id="content-section">
+    <div class="content-sections">
+        <div class="content-levels">
+            <span class="content-headings">
+                <div class="content-headings-icons">
+                    <span class="content-heading-inner-icon">
+                        <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+                    </span>
+                </div>
+                &nbsp; &nbsp; &nbsp;
+                <h2>
+                    Profile
+                </h2>
+            </span>
+            <p>
+                Enthusiastic developer looking forward to challenges in the field of Engineering
+            </p>
+        </div>
+
+        <div class="content-levels">
+            <span class="content-headings">
+                <div class="content-headings-icons">
+                    <span class="content-heading-inner-icon">
+                        <i class="fa fa-cogs fa-2x" aria-hidden="true"></i>
+                    </span>
+                </div>
+                &nbsp; &nbsp; &nbsp;
+                <h2>
+                    Skills
+                </h2>
+            </span>
+            <ul class="skills-ul" style="list-style: none;">
+                <h3 style="text-align: center;">
+                    Technical Skills
+                </h3>
+                <li>
+                    <div  class="skill-li-name">Angular</div> 
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div> 
+                    
+                </li>
+                <li>
+                    <div  class="skill-li-name"> Socket.io  </div> 
+                     
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div> 
+                </li>
+                <li>
+                    <div  class="skill-li-name">NodeJs</div> 
+                     
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div>    
+                </li>
+                <li>
+                    <div  class="skill-li-name">MongodB</div> 
+                    
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div> 
+                </li>
+                <li>
+                    <div  class="skill-li-name">ExpressJs</div> 
+                        
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div> 
+                </li>
+                <li>
+                    <div  class="skill-li-name">Ionic</div> 
+                      
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div>  
+                </li>
+                <li>
+                    <div  class="skill-li-name">MySql</div> 
+                    
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 50%"></span></div>
+                    </div> 
+                </li>
+                <li>
+                    <div  class="skill-li-name">HTML5</div> 
+                      
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div>   
+                </li>
+                <li>
+                    <div  class="skill-li-name">CSS</div> 
+                        
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div> 
+                </li>
+                <li>
+                    <div  class="skill-li-name">AJAX</div> 
+                       
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div>  
+                </li>
+                <li>
+                    <div  class="skill-li-name">JavaScript</div> 
+                        
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div> 
+                </li>
+                <li>
+                    <div  class="skill-li-name">JQuery</div> 
+                    
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 75%"></span></div>
+                    </div> 
+                </li>
+                <li>
+                    <div  class="skill-li-name">Python</div> 
+                    
+                    <div class="skill-level">
+                        <div class="meter"><span style="width: 70%"></span></div>
+                    </div> 
+                </li>
+            </ul>
+            
+        </div>
+        
+
+    </div>
+    <hr>
+    <div class="content-sections">
+        <div class="content-levels">
+            <span class="content-headings">
+                <div class="content-headings-icons">
+                    <span class="content-heading-inner-icon">
+                        <i class="fa fa-briefcase fa-2x" aria-hidden="true"></i>
+                    </span>
+                </div>
+                &nbsp; &nbsp; &nbsp;
+                <h2>
+                    Work Experience
+                </h2>
+            </span>
+            <h3>MEAN Stack Developer, Ikomet Technology Pvt Ltd (02/2018 – Present) Chennai, India</h3>
+            <h4>Achievements/Tasks</h4>
+            <ul>
+               <li>
+                Working as a MEAN Stack developer in making a Workflow application for e publishing automation industry.    
+               </li> 
+               <li>
+                Created the front end and backend from Scratch to operational release stage with very minimal bugs.
+               </li>
+            </ul>
+        </div>
+        <div class="content-levels">
+            <span class="content-headings">
+                <div class="content-headings-icons">
+                    <span class="content-heading-inner-icon">
+                        <i class="fa fa-book fa-2x" aria-hidden="true"></i>
+                    </span>
+                </div>
+                &nbsp; &nbsp; &nbsp;
+                <h2>
+                    Education
+                </h2>
+            </span>
+            <h3>Bachelor of Engineering in Mechanical at Loyola ICAM College of Engineering and Technology, 7.41 CPGA (06/14 – 04/2017)</h3>
+            <h4>Project</h4>
+            <ul>
+               <li>
+                Design and Analysis of Ducted Wind Turbine
+               </li>                        
+            </ul>
+            <h3>HSE –TamilNadu State Board at MCC Matriculation and Higher Secondary School, 85.33% (06/11 – 03/2013)</h3>
+            <h4>Group</h4>
+            <ul>
+               <li>
+                Bio-Maths
+               </li>                        
+            </ul>
+            
+        </div>
+
+    </div>
+</section>
+
+
+</main>`
+document.body.innerHTML = resumeInnerHTML;
